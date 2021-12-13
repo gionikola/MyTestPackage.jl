623910f91c33ac9c586aff337add6a4cbc2fc050
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2021-12-13 12:02:10 -0800

    Add Documenter to `make.jl`

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,17 @@
 # Inside make.jl
-push!(LOAD_PATH,"../src/")
+push!(LOAD_PATH, "../src/")
 using MyTestPackage
 
+import Pkg; Pkg.add("Documenter");
 using Documenter
 
 makedocs(
-         sitename = "MyTestPackage.jl",
-         modules  = [MyTestPackage],
-         pages=[
-                "Home" => "index.md"
-               ])
-               
+    sitename = "MyTestPackage.jl",
+    modules = [MyTestPackage],
+    pages = [
+        "Home" => "index.md"
+    ])
+
 deploydocs(;
-    repo="github.com/gionikola/MyTestPackage.jl",
+    repo = "github.com/gionikola/MyTestPackage.jl"
 )
